2b30fbc875cd355e8d035d5025474e351bddcc11
Alberto Juan Sebastian <albertojuansebastian@gmail.com>
2019-11-17 23:17:29 +0100

    Bigger input

--- main.m
+++ main.m
@@ -1,5 +1,20 @@
 clear all;
 delete coder_output;
 delete decoder_output;
-codificadorLZ78_Sebastian_Lombranna_Alberto('input','coder_output');
+codificadorLZ78_Sebastian_Lombranna_Alberto('input2','coder_output');
 decodificadorLZ78_Sebastian_Lombranna_Alberto('coder_output','decoder_output');
+
+input_file_id = fopen('input2', 'r');
+[i, count] = fread(input_file_id,'ubit8');
+input_bits = count * 8
+fclose(input_file_id );
+
+input_file_id = fopen('coder_output', 'r');
+[i, count] = fread(input_file_id,'ubit8');
+coder_output_bits = count * 8
+fclose(input_file_id );
+
+input_file_id = fopen('decoder_output', 'r');
+[i, count] = fread(input_file_id,'ubit8');
+decoder_output_bits = count * 8
+fclose(input_file_id );
