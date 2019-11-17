43e83e5d057f194bcc52f39ab04c70e02156aee7
Alberto Juan Sebastian <albertojuansebastian@gmail.com>
2019-11-17 21:56:41 +0100

    decoder LZ78

--- main.m
+++ main.m
@@ -1,4 +1,5 @@
 clear all;
 delete coder_output;
+delete decoder_output;
 codificadorLZ78_Sebastian_Lombranna_Alberto('input','coder_output');
 decodificadorLZ78_Sebastian_Lombranna_Alberto('coder_output','decoder_output');
